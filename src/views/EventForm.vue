62384703448a9a00841f1fa7e5dfa317e5165a1c
konnRG <burussakorn_ruangon@elearning.cmu.ac.th>
2022-10-19 19:23:32 +0700

    2.10 Display photo on EventDetailView.

--- src/views/EventForm.vue
+++ src/views/EventForm.vue
@@ -52,37 +52,38 @@ export default {
         title: '',
         description: '',
         location: '',
-        organizer: { id: '', name: '' }
+        organizer: { id: '', name: '' },
+        imageUrls: []
       },
       files: []
     }
   },
   methods: {
     saveEvent() {
+      console.log(this.files)
       Promise.all(
         this.files.map((file) => {
           return EventService.uploadFile(file)
         })
       ).then((response) => {
-        console.log(response)
-        console.log('finish upload file')
-      })
-      EventService.saveEvent(this.event)
-        .then((response) => {
-          console.log(response)
-          this.$router.push({
-            name: 'EventLayoutView',
-            params: { id: response.data.id }
+        this.event.imageUrls = response.map((r) => r.data)
+        EventService.saveEvent(this.event)
+          .then((response) => {
+            console.log(response)
+            this.$router.push({
+              name: 'EventDetails',
+              params: { id: response.data.id }
+            })
+            this.GStore.flashMessage =
+              'You are succcessfully add a new event for ' + response.data.title
+            setTimeout(() => {
+              this.GStore.flashMessage = ''
+            }, 3000)
           })
-          this.GStore.flashMessage =
-            'You are succcessfully add a new event for ' + response.data.title
-          setTimeout(() => {
-            this.GStore.flashMessage = ''
-          }, 3000)
-        })
-        .catch(() => {
-          this.$router.push('NetworkError')
-        })
+          .catch(() => {
+            this.$router.push('NetworkError')
+          })
+      })
     },
     handleImages(files) {
       this.files = files
